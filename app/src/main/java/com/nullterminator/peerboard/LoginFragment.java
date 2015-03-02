8214655128b55021c83721024155d8b0fe1b8e37
Rayun Mehrab <rayun.mehrab@mail.utoronto.ca>
2015-03-01 19:14:34 -0500

    Added more methods to LoginFragment.java

--- app/src/main/java/com/nullterminator/peerboard/LoginFragment.java
+++ app/src/main/java/com/nullterminator/peerboard/LoginFragment.java
@@ -46,7 +46,26 @@ import java.util.List;
 
 public class LoginFragment extends Fragment implements View.OnClickListener {
 
-    protected Activity mActivity;
+    int veriPIN = 0;
+	String name = "";
+	private Toolbar toolbar;
+	View view;
+	//Comment 1 - Feb 18 6:15pm
+    //Comment 2 - Feb 18 6:18pm
+    //Comment 3 - Feb 18 9:19pm
+    //Comment 4 - Feb 22 9:47pm
+	//Comment 5 - Feb 22 10:20pm
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+	protected Activity mActivity;
     private ImageView mCamera;
     private TextView intro;
 
@@ -72,19 +91,46 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_login, container, false);
+        view = inflater.inflate(R.layout.activity_login, container, false);
 
-        intro = (TextView)view.findViewById(R.id.intro);
+		/*toolbar = (Toolbar) view.findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        view.setSupportActionBar(toolbar);*/ //TODO: Make materiYOLO work later
 
-        Typeface typeFace = Typeface.createFromAsset(mActivity.getAssets(), "fonts/ArchitectsDaughter.ttf");
-        intro.setTypeface(typeFace);
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) view.findViewById(R.id.email);
+        populateAutoComplete();
 
-        intro.setText(Html.fromHtml(mActivity.getString(R.string.introd)));
+        mPasswordView = (EditText) view.findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+				@Override
+				public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+					if (id == R.id.login || id == EditorInfo.IME_NULL) {
+						attemptLogin();
+						return true;
+					}
+					return false;
+				}
+			});
 
-        mCamera = (ImageView)view.findViewById(R.id.camera);
+        Button mEmailSignInButton = (Button) view.findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					attemptLogin();
+				}
+			});
 
-        mCamera.setOnClickListener(this);
+		Button mEmailSignUpButton = (Button) view.findViewById(R.id.email_sign_up_button);
+        mEmailSignUpButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					//attemptSignUp();
+				}
+			});
 
+        mLoginFormView = view.findViewById(R.id.login_form);
+        mProgressView = view.findViewById(R.id.login_progress);
+		
         return view;
     }
 
@@ -92,5 +138,275 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     public void onClick(View v)
     {
         //do whatever you want here
+    }
+	
+	private void populateAutoComplete() {
+        getLoaderManager().initLoader(0, null, view);
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    public void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+		if (TextUtils.isEmpty(password)){
+			mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = mPasswordView;
+			cancel = true;
+		}
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+			try {
+				Context context = getApplicationContext();
+				CharSequence text = "Will attempt jdbc now";
+				int duration = Toast.LENGTH_SHORT;
+
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@")&&email.contains("mail.utoronto.ca");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+				show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+					}
+				});
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+				show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+					}
+				});
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(view,
+								// Retrieve data rows for the device user's 'profile' contact.
+								Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+													 ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+
+								// Select only email addresses.
+								ContactsContract.Contacts.Data.MIMETYPE +
+								" = ?", new String[]{ContactsContract.CommonDataKinds.Email
+									.CONTENT_ITEM_TYPE},
+
+								// Show primary email addresses first. Note that there won't be
+								// a primary email address if the user hasn't specified one.
+								ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<String>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
+
+        addEmailsToAutoComplete(emails);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+
+    }
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+			ContactsContract.CommonDataKinds.Email.ADDRESS,
+			ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+			new ArrayAdapter<String>(LoginFragment.view,
+									 android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        mEmailView.setAdapter(adapter);
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            String salt;
+            String pword;
+            String pwd = "";
+
+
+
+            // TODO: attempt authentication against a network service.
+            try {
+                salt = Sha1Hash.SHA1(mEmail);
+                pword = Sha1Hash.SHA1(mPassword);
+                pwd = Sha1Hash.SHA1(salt + pword);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                veriPIN = DBCompare.authenticateLogin(mEmail, pwd);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+			try {
+				if ((veriPIN == -1)||(veriPIN == -2)){
+					return false;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+
+
+            //Rayun: code for database communication
+
+            /*for (String credential : DUMMY_CREDENTIALS) {
+			 String[] pieces = credential.split(":");
+			 if (pieces[0].equals(mEmail)) {
+			 // Account exists, return true if the password matches.
+			 return pieces[1].equals(mPassword);
+			 }
+			 }*/
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+			//TODO: need to understand this part and write the post execute part.
+
+            if (success) {
+				Context context = getApplicationContext();
+				CharSequence text = "Successfully logged in, PIN = " + veriPIN;
+				int duration = Toast.LENGTH_SHORT;
+
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+                finish();
+            } else {
+				Context context = getApplicationContext();
+				CharSequence text = "Email invalid/not registered " + veriPIN;
+				int duration = Toast.LENGTH_SHORT;
+
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
     }
 }
