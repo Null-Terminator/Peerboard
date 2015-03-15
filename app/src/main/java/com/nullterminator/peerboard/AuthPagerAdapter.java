a4c3c19e220ca0666cd121bb4937e75b7afb1d57
Rayun Mehrab <rayun.mehrab@mail.utoronto.ca>
2015-03-15 14:38:29 -0400

    Added everything necessary, but broke everything (run time error). Will debug on phone.

--- app/src/main/java/com/nullterminator/peerboard/AuthPagerAdapter.java
+++ app/src/main/java/com/nullterminator/peerboard/AuthPagerAdapter.java
@@ -7,28 +7,50 @@ import com.google.android.gms.internal.*;
 
 public class AuthPagerAdapter extends FragmentStatePagerAdapter {
 
-    public AuthPagerAdapter(FragmentManager fm)
-    {
+	
+	CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+
+    // Build a Constructor and assign the passed Values to appropriate values in the class
+    public AuthPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
         super(fm);
+
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+
     }
+
+    //This method return the fragment for the every position in the View Pager
+    @Override
+    public Fragment getItem(int position) {
+
+        if(position == 0) // if the position is 0 we are returning the First tab
+        {
+            LoginFragment logintab = new LoginFragment();
+            return logintab;
+        }
+        else             // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
+        {
+            SignUpFragment signuptab = new SignUpFragment();
+            return signuptab;
+        }
+
+
+    }
+
+    // This method return the titles for the Tabs in the Tab Strip
 
     @Override
-    public android.support.v4.app.Fragment getItem(int index)
-    {
-	    switch (index)
-		{
-			case 0:
-				return new LoginFragment();
-			case 1:
-				return new SignUpFragment();
-		}
-
-        return null;
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
     }
+
+    // This method return the Number of tabs for the tabs Strip
 
     @Override
-    public int getCount()
-    {
-        return 2;
+    public int getCount() {
+        return NumbOfTabs;
     }
+	
 }
